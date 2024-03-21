308b9ff38b6cb34c3140c6df252f2dd3c1821b7c
Harsh Mriduhash <52977247+harshmriduhash@users.noreply.github.com>
2024-03-21 22:07:35 +0530

    New

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -51,7 +51,7 @@ const HomeInfo = ({ currentStage }) => {
       <div className="info-box">
         <p className="font-medium sm:text-xl text-center">
           If You Need a project done or you're hiring for a developer? <br />{" "}
-          I'm just a message away
+          I'm just one message away
         </p>
         <Link to="/contact" className="neo-brutalism-white neo-btn">
           Let's talk
