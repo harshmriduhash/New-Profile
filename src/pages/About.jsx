18ec81e00c686309b2d838010b2ea9803275c9a0
Harsh Mriduhash <52977247+harshmriduhash@users.noreply.github.com>
2024-03-22 23:46:21 +0530

    New

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
       <div className="mt-5 flex flex-col gap-3 text-slate-500">
         <p>
           MERN stack Developer with 6 years of experience in taking businesses
-          from O to 1. An excellent way of storytelling through my technical
+          from zero to 1. An excellent way of storytelling through my technical
           expertise. Specialized in building products from scratch and scaling
           it to handle loads of users. Making it more reliable and optimised for
           higher performance.
