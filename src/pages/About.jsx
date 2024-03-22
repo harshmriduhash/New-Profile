3d347552af0d1a173cff21862d2e985c143e253c
Harsh Mriduhash <52977247+harshmriduhash@users.noreply.github.com>
2024-03-22 23:43:13 +0530

    New

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -56,8 +56,8 @@ const About = () => {
             I Have worked with multiple startups in different domains/sector
             such as TravelTech, MusicTech, Healthcare. while working with them i
             have gained hands on experience on multiple tech stack such as
-            JavaScript, MERN, TypeScript, Python, Django, Java, AWS Here's the
-            rundown:-
+            JavaScript, MERN, TypeScript, Next.js, Python, Django, Java, AWS
+            Here's the rundown:-
           </p>
         </div>
 
