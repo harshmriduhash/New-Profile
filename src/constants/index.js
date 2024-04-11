e5ef9016da3eb4451ae8da590230db25ad40fb34
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2024-04-11 23:55:40 +0530

    New

--- src/constants/index.js
+++ src/constants/index.js
@@ -123,7 +123,7 @@ export const experiences = [
     iconBg: "#accbe1",
     date: "2024 - Present",
     points: [
-      "Currently working at MusicTech company. Building the product on MERN stack from scratch",
+      "Currently working at HR-Tech company. Building the product on MERN stack from scratch",
       "Build the entire architecture of the product while working directly with Founder",
       "A platform that is focused on creating a unique and personalized experience for music lovers.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
