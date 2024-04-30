00c0a00326b0b402d2d2a79d9de8c8583c34b704
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2024-04-30 11:13:37 +0530

    Yes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -54,7 +54,7 @@ const About = () => {
             I Have worked with multiple startups in different domains/sector
             such as TravelTech, MusicTech, Healthcare. while working with them i
             have gained hands on experience on multiple tech stack such as
-            JavaScript, React.js TypeScript, Next.js, React Native, Flutter, System
+            JavaScript, React.js, node.js TypeScript, Next.js, React Native, Flutter, System
             Design, AWS. Here's the rundown:-
           </p>
         </div>
